b534d9db886c809f05f55467e2844a0e1c8c74ba
Lukasz Tomczok <lukasztomczok@gmail.com>
2019-10-02 14:56:19 +0200

    complete the pool properties

--- API/model/pool.js
+++ API/model/pool.js
@@ -4,8 +4,26 @@ const Pool = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
       primaryKey: true
     },
+    name: {
+      type: Sequelize.STRING
+    },
     openTime: {
       type: Sequelize.STRING
+    },
+    closeTime: {
+      type: Sequelize.STRING
+    },
+    exitTime: {
+      type: Sequelize.STRING
+    },
+    maximumNumberOfLanes: {
+      type: Sequelize.INTEGER
+    },
+    width: {
+      type: Sequelize.INTEGER
+    },
+    length: {
+      type: Sequelize.INTEGER
     }
   }, {
       tableName: 'SwimmingPools'
